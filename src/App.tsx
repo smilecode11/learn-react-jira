c961a936efbab54280e629e8afdb786f06a22563
尘心 <smilewupeng@gmail.com>
2022-04-13 22:01:08 +0800

    ci: 初始化项目, 约束 git commit, 配置 eslint, prettire

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import './App.css'
 function App() {
 	return (
 		<div className="App">
-			<h3>React</h3><p>
+			<h3>React</h3>
+			<p>
 				react project<span>初始化react项目</span>
 			</p>
 			<p>对的对的</p>
