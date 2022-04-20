6744211bc8956bf97f1748ff927a3480f520799a
尘心 <smilewupeng@gmail.com>
2022-04-20 17:29:07 +0800

    feat: pin 组件封装 & useEditProject 操作(取)关注

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,6 +1,8 @@
 import { Table, TableProps } from 'antd'
+import { Pin } from 'components/pin'
 import dayjs from 'dayjs'
 import { Link } from 'react-router-dom'
+import { useEditProject } from 'utils/project'
 
 export interface User {
 	id: number
@@ -17,6 +19,7 @@ export interface Project {
 	personId: number
 	organization: string //	部门
 	created: number //	时间
+	pin: boolean //	收藏
 }
 
 interface ListPros extends TableProps<Project> {
@@ -24,14 +27,27 @@ interface ListPros extends TableProps<Project> {
 }
 
 const List = ({ users, ...props }: ListPros) => {
+	const { mutate } = useEditProject()
+
+	// const pinProject = (id: number, pin: boolean) => mutate({ id, pin })
+	//	函数式编程写法
+	const pinProject = (id: number) => (pin: boolean) => mutate({ id, pin })
+
 	return (
 		<Table
 			rowKey={'id'}
 			pagination={false}
 			columns={[
 				{
+					title: <Pin checked={true} disabled={true} />,
+					render(value, project) {
+						// return <Pin checked={project.pin} onCheckedChange={pin => pinProject(project.id, pin)} />
+						//	函数式编程写法
+						return <Pin checked={project.pin} onCheckedChange={pinProject(project.id)} />
+					},
+				},
+				{
 					title: '项目名称',
-					key: 'name',
 					render(value, project) {
 						return <Link to={`/projects/${String(project.id)}`}>{project.name}</Link>
 					},
@@ -39,18 +55,15 @@ const List = ({ users, ...props }: ListPros) => {
 				{
 					title: '部门',
 					dataIndex: 'organization',
-					key: 'organization',
 				},
 				{
 					title: '时间',
-					key: 'created',
 					render(value, project) {
 						return <span>{project.created ? dayjs(project.created).format('YYYY-MM-DD') : '无'}</span>
 					},
 				},
 				{
 					title: '负责人',
-					key: 'personId',
 					render(value, project) {
 						return <span>{users.find(user => user.id === project.personId)?.name || '未知'}</span>
 					},
--- src/utils/project.ts
+++ src/utils/project.ts
@@ -4,6 +4,7 @@ import { cleanObject } from 'utils'
 import { useHttp } from './http'
 import { useAsync } from './use-async'
 
+/** 获取列表*/
 export const useProjects = (param?: Partial<Project>) => {
 	const client = useHttp()
 	const { run, ...result } = useAsync<Project[]>()
@@ -13,4 +14,44 @@ export const useProjects = (param?: Partial<Project>) => {
 	}, [param])
 
 	return result
+}
+
+/** 编辑项目*/
+export const useEditProject = () => {
+	const client = useHttp()
+	const { run, ...aysncResult } = useAsync<Project>()
+
+	const mutate = (param: Partial<Project>) => {
+		return run(
+			client(`projects/${param.id}`, {
+				method: 'PATCH',
+				data: param,
+			})
+		)
+	}
+
+	return {
+		mutate,
+		...aysncResult,
+	}
+}
+
+/** 新建项目*/
+export const useAddProject = () => {
+	const client = useHttp()
+	const { run, ...asyncResult } = useAsync()
+
+	const mutate = (param: Partial<Project>) => {
+		return run(
+			client(`projects`, {
+				method: 'POST',
+				data: param,
+			})
+		)
+	}
+
+	return {
+		mutate,
+		...asyncResult,
+	}
 }
